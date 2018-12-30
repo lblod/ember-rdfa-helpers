c131c8701680d29a8255374b44495a21a3c7c56f
Aad Versteden <madnificent@gmail.com>
2018-12-30 09:27:46 +0100

    Upgrade to new test structure
    Our tests aren't functional, but we're moving ahead so we can upgrade
    them when desired.

--- tests/integration/components/rdfa/ctx-each-resource-test.js
+++ tests/integration/components/rdfa/ctx-each-resource-test.js
@@ -1,24 +1,26 @@
-import { moduleForComponent, test } from 'ember-qunit';
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render, find } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('rdfa/ctx-each-resource', 'Integration | Component | rdfa/ctx each resource', {
-  integration: true
-});
+module('Integration | Component | rdfa/ctx each resource', function(hooks) {
+  setupRenderingTest(hooks);
 
-test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+  test('it renders', async function(assert) {
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{rdfa/ctx-each-resource}}`);
+    await render(hbs`{{rdfa/ctx-each-resource}}`);
 
-  assert.equal(this.$().text().trim(), '');
+    assert.equal(find('*').textContent.trim(), '');
 
-  // Template block usage:
-  this.render(hbs`
-    {{#rdfa/ctx-each-resource}}
-      template block text
-    {{/rdfa/ctx-each-resource}}
-  `);
+    // Template block usage:
+    await render(hbs`
+      {{#rdfa/ctx-each-resource}}
+        template block text
+      {{/rdfa/ctx-each-resource}}
+    `);
 
-  assert.equal(this.$().text().trim(), 'template block text');
+    assert.equal(find('*').textContent.trim(), 'template block text');
+  });
 });
--- tests/integration/components/with-rdfa-resource-test.js
+++ tests/integration/components/with-rdfa-resource-test.js
@@ -1,24 +1,26 @@
-import { moduleForComponent, test } from 'ember-qunit';
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render, find } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('with-rdfa-resource', 'Integration | Component | with rdfa resource', {
-  integration: true
-});
+module('Integration | Component | with rdfa resource', function(hooks) {
+  setupRenderingTest(hooks);
 
-test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+  test('it renders', async function(assert) {
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{with-rdfa-resource}}`);
+    await render(hbs`{{with-rdfa-resource}}`);
 
-  assert.equal(this.$().text().trim(), '');
+    assert.equal(find('*').textContent.trim(), '');
 
-  // Template block usage:
-  this.render(hbs`
-    {{#with-rdfa-resource}}
-      template block text
-    {{/with-rdfa-resource}}
-  `);
+    // Template block usage:
+    await render(hbs`
+      {{#with-rdfa-resource}}
+        template block text
+      {{/with-rdfa-resource}}
+    `);
 
-  assert.equal(this.$().text().trim(), 'template block text');
+    assert.equal(find('*').textContent.trim(), 'template block text');
+  });
 });
