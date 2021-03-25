30aa336aa7df014c2cca253714f549750fc57cb5
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2021-03-25 16:00:07 +0100

    fix: Make sure the is-rdfa-resource util always returns a boolean

--- addon/utils/is-rdfa-resource.js
+++ addon/utils/is-rdfa-resource.js
@@ -1,5 +1,5 @@
 export function isRdfaResource(value) {
-  return value && typeof value === "object" && !(value instanceof Date);
+  return Boolean(value && typeof value === "object" && !(value instanceof Date));
 }
 
 export default isRdfaResource;
