c136154fb85133a141f3bef3c3f72d12d35b60af
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2021-05-05 15:19:56 +0200

    Rework the <Rdfa::CtxImg> component

--- addon/components/rdfa/ctx-img.js
+++ addon/components/rdfa/ctx-img.js
@@ -1,13 +1,3 @@
-import Component from '@ember/component';
-import layout from '../../templates/components/rdfa/ctx-img';
-import StandardRdfaComponent from '../../mixins/rdfa/standard';
-import { computed } from '@ember/object';
+import templateOnly from '@ember/component/template-only';
 
-export default Component.extend(StandardRdfaComponent, {
-  layout,
-  tagName: 'img',
-  attributeBindings: ['src', 'rdfaProperty:property', 'alt', 'width', 'height', 'srcset', 'sizes'],
-  src: computed('value', function() {
-    return this.value && this.value.uri ? this.value.uri : this.value;
-  })
-});
+export default templateOnly();
