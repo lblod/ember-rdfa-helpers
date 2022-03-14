853cc96c88480df98c3073393cca75b48d549d26
“[BenJay10]” <[benbehaeghe@gmail.com]>
2022-03-14 16:13:28 +0100

    Extended testing on LinkTo component
    Noticed some todo's on the link-to test, and implemented them, also
    adding tests for the overrideUri feature.

--- tests/integration/components/rdfa/link-to-test.js
+++ tests/integration/components/rdfa/link-to-test.js
@@ -6,15 +6,41 @@ import hbs from 'htmlbars-inline-precompile';
 module('Integration | Component | rdfa/link-to', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function (assert) {
+  test('basic usage', async function (assert) {
     const storeService = this.owner.lookup('service:store');
-
     let project = storeService.createRecord('project', {
       uri: 'https://github.com/lblod/ember-rdfa-helpers',
       name: 'ember-rdfa-helpers',
       id: '1234',
     });
+    this.set('project', project);
 
+    await render(hbs`
+      <Rdfa::LinkTo
+        class="someClass"
+        @value={{this.project}}
+      >
+        basic usage
+      </Rdfa::LinkTo>
+    `);
+
+    assert
+      .dom('a')
+      .hasText('basic usage')
+      .hasClass('someClass') //testing ...attributes
+      .doesNotHaveAttribute('href')
+      .doesNotHaveAttribute('property')
+      .hasAttribute('typeof', 'http://xmlns.com/foaf/0.1/Thing')
+      .hasAttribute('resource', 'https://github.com/lblod/ember-rdfa-helpers');
+  });
+
+  test('use uri', async function (assert) {
+    const storeService = this.owner.lookup('service:store');
+    let project = storeService.createRecord('project', {
+      uri: 'https://github.com/lblod/ember-rdfa-helpers',
+      name: 'ember-rdfa-helpers',
+      id: '1234',
+    });
     this.set('project', project);
 
     await render(hbs`
@@ -24,9 +50,29 @@ module('Integration | Component | rdfa/link-to', function (hooks) {
       >
         useUri
       </Rdfa::LinkTo>
+    `);
 
-      {{!-- TODO
+    assert
+      .dom('a')
+      .hasText('useUri')
+      .hasAttribute('href', 'https://github.com/lblod/ember-rdfa-helpers')
+      .doesNotHaveAttribute('property')
+      .hasAttribute('typeof', 'http://xmlns.com/foaf/0.1/Thing')
+      .doesNotHaveAttribute('resource');
+  });
+
+  test('using link-to and href-to', async function (assert) {
+    const storeService = this.owner.lookup('service:store');
+    let project = storeService.createRecord('project', {
+      uri: 'https://github.com/lblod/ember-rdfa-helpers',
+      name: 'ember-rdfa-helpers',
+      id: '1234',
+    });
+    this.set('project', project);
+
+    await render(hbs`
         <Rdfa::LinkTo
+          data-test-linkto
           @value={{this.project}}
           @link-to="projects.show"
         >
@@ -34,18 +80,104 @@ module('Integration | Component | rdfa/link-to', function (hooks) {
         </Rdfa::LinkTo>
 
         <Rdfa::LinkTo
+          data-test-hrefto
           @value={{this.project}}
           @href-to={{href-to "projects.show" "1234"}}
         >
           href-to
         </Rdfa::LinkTo>
-      --}}
+
+        <Rdfa::LinkTo
+          data-test-linktoUri
+          @value={{this.project}}
+          @link-to="projects.show"
+          @useUri={{true}}
+        >
+          link-to-uri
+        </Rdfa::LinkTo>
+
+        <Rdfa::LinkTo
+          data-test-hreftoUri
+          @value={{this.project}}
+          @href-to={{href-to "projects.show" "1234"}}
+          @useUri={{true}}
+        >
+          href-to-uri
+        </Rdfa::LinkTo>
     `);
 
     assert
-      .dom('a')
-      .hasText('useUri')
+      .dom('[data-test-linkto]')
+      .hasText('link-to')
+      .hasAttribute('href', '/projects/1234')
+      .doesNotHaveAttribute('property')
+      .hasAttribute('typeof', 'http://xmlns.com/foaf/0.1/Thing')
+      .hasAttribute('resource', 'https://github.com/lblod/ember-rdfa-helpers');
+    assert
+      .dom('[data-test-hrefto]')
+      .hasText('href-to')
+      .hasAttribute('href', '/projects/1234')
+      .doesNotHaveAttribute('property')
+      .hasAttribute('typeof', 'http://xmlns.com/foaf/0.1/Thing')
+      .hasAttribute('resource', 'https://github.com/lblod/ember-rdfa-helpers');
+    assert
+      .dom('[data-test-linktoUri]')
+      .hasText('link-to-uri')
       .hasAttribute('href', 'https://github.com/lblod/ember-rdfa-helpers')
-      .hasAttribute('typeof', 'http://xmlns.com/foaf/0.1/Thing');
+      .doesNotHaveAttribute('property')
+      .hasAttribute('typeof', 'http://xmlns.com/foaf/0.1/Thing')
+      .doesNotHaveAttribute('resource');
+    assert
+      .dom('[data-test-hreftoUri]')
+      .hasText('href-to-uri')
+      .hasAttribute('href', 'https://github.com/lblod/ember-rdfa-helpers')
+      .doesNotHaveAttribute('property')
+      .hasAttribute('typeof', 'http://xmlns.com/foaf/0.1/Thing')
+      .doesNotHaveAttribute('resource');
+  });
+
+  test('override uri', async function (assert) {
+    const storeService = this.owner.lookup('service:store');
+    let project = storeService.createRecord('project', {
+      uri: 'https://github.com/lblod/ember-rdfa-helpers',
+      name: 'ember-rdfa-helpers',
+      id: '1234',
+    });
+    this.set('project', project);
+
+    await render(hbs`
+        <Rdfa::LinkTo
+          data-test-linkto
+          @value={{this.project}}
+          @link-to="projects.show"
+          @overrideUri={{true}}
+        >
+          link-to
+        </Rdfa::LinkTo>
+
+        <Rdfa::LinkTo
+          data-test-hrefto
+          @value={{this.project}}
+          @href-to={{href-to "projects.show" "1234"}}
+          @overrideUri={{true}}
+        >
+          href-to
+        </Rdfa::LinkTo>
+    `);
+
+    assert
+      .dom('[data-test-linkto]')
+      .hasText('link-to')
+      .hasAttribute('href', '/projects/1234')
+      .doesNotHaveAttribute('property')
+      .hasAttribute('typeof', 'http://xmlns.com/foaf/0.1/Thing')
+      .hasAttribute('resource', '/projects/1234');
+    assert
+      .dom('[data-test-hrefto]')
+      .hasText('href-to')
+      .hasAttribute('href', '/projects/1234')
+      .doesNotHaveAttribute('property')
+      .hasAttribute('typeof', 'http://xmlns.com/foaf/0.1/Thing')
+      .hasAttribute('resource', '/projects/1234');
   });
 });
